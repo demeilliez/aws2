d662483165cfe30e1ff566368a0c70b527ce59f6
demeilliez <c.demeilliez@gmail.com>
2017-09-29 14:14:02 +0200

    update

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$url = "https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49";
+$raw = file_get_contents($url);
+var_dump($raw);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Gibli</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+</head>
+<body>
+    <p>J'aime les gibli ! et oui</p>
+    toto ou non ?
+    test number 8
+    Il faut que ça marche, c'est le dernier test
+    Encore un en fait, pour le fun
+    Pourquoi tu marches pas
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ stacktest.php
@@ -0,0 +1,11 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class DependencyFailureTest extends TestCase
+{
+    public function testOne()
+    {
+        $this->assertTrue(true);
+    }
+
+}
